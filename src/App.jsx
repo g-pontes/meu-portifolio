5e4287426213fa775dcd57d5316db3b7904a9169
Mostyzx <gui12072004@gmail.com>
2024-03-25 17:11:31 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import "./index.css"
+import Header from "./components/header/Header"
+
+const App = () => {
+  return (
+    <>
+    <Header />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import "./header.css"
+
+function header() {
+  return (
+    <header className="header">
+      <nav className="nav container">
+        <a href="index.html" className="nav__logo">Smith</a>
+
+        <div className="nav__menu">
+          <ul className="nav__list grid">
+            <li className="nav__item">
+              <a href="index.html" className="nav__link">
+                <i className="uil uil-estate nav__icon"></i> Inicio
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="index.html" className="nav__link">
+                <i className="uil uil-user nav__icon"></i> Sobre
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="index.html" className="nav__link">
+                <i className="uil uil-file-alt nav__icon"></i> Skills
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="index.html" className="nav__link">
+                <i className="uil uil-scenery nav__icon"></i> Portifolio
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="index.html" className="nav__link">
+                <i className="uil uil-message nav__icon"></i> Contato
+              </a>
+            </li>
+          </ul>
+
+          <i className="uil uil-times nav__close"></i>
+        </div>
+        <div className="nav__toggle">
+          <i class="uil uil-align-right"></i>
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default header
